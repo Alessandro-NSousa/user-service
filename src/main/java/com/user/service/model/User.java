bf53f272dd48f0ab66dea924d3a180c69136a013
Alessandro-NSousa <allessandro188@gmail.com>
2026-09-10 23:00:59 -0300

    Configurações de banco

---
+++ src/main/java/com/user/service/model/User.java
@@ -0,0 +1,23 @@
+package com.user.service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "User")
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+}
