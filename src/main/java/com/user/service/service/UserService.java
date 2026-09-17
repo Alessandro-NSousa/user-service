150d50bd3dfd6b72023f17ea3c8973ff8a9b476d
Alessandro-NSousa <allessandro188@gmail.com>
2026-09-16 23:08:40 -0300

    Ajuste na injeção de dependecia e no path do controller  de user

--- src/main/java/com/user/service/service/UserService.java
+++ src/main/java/com/user/service/service/UserService.java
@@ -2,16 +2,17 @@ package com.user.service.service;
 
 import com.user.service.model.User;
 import com.user.service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public List<User> findAll() {
         return userRepository.findAll();
